a4a87d9db5c3b0e1a94c6c3d10a5e7ccab56e007
Otero <oteronickbrian@gmail.com>
2024-04-19 00:16:56 +0300

    Update CRUD operations

--- chapter6/planner/models/users.py
+++ chapter6/planner/models/users.py
@@ -4,3 +4,25 @@ from beanie import Document, Link
 from pydantic import BaseModel, EmailStr
 from models.events import Event
 
+
+class user(Document):
+    email: EmailStr
+    password: str
+    events: Optional[List[Link[Event]]]
+
+    class Settings:
+        name = "users"
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com",
+                "password": "strong!!!",
+                "events": [],
+            }
+        }
+
+
+class UserSignIn(BaseModel):
+    email: EmailStr
+    password: str
